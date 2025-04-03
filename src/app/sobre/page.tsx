36534f417977552d6045e583094ece5c7e15e1be
Emanuela Caetano <113469497+EmanuelaCaetano@users.noreply.github.com>
2025-04-03 02:07:13 -0300

    commit inicial

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,12 +1,9 @@
-import Image from "next/image";
-import Footer from "@/components/footer";
 import Navbar from "@/components/navBar";
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
-      <Footer/>
     </div>
   );
 }
